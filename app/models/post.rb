7f6f63c9c21de7be1109084a0b439cf864b35f6d
Donnell A Rawlings <donnierawlings@gmail.com>
2013-01-01 01:43:37 -0500

    cool u man

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,4 +18,13 @@ class Post < ActiveRecord::Base
     validates_attachment_content_type :picture, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
 
 	validates_attachment_size :picture, :less_than => 4.megabyte
+
+    def previous_upload
+      self.class.first(:conditions => ["created_at  < ? ", created_at ], :order => "created_at desc")
+    end
+
+    def next_upload
+      self.class.first(:conditions => ["created_at  > ? ", created_at  ], :order => "created_at asc")
+    end
+
 end
